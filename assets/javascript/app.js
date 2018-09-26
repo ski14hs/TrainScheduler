98c278d9e970315dc207e8f60fd35d74fc4980a6
ski14hs <sean.k.ishikawa@gmail.com>
2018-09-25 18:26:13 -0700

    index file set up

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+
+//Initialize Firebase
+
+// Initialize Firebase
+var config = {
+apiKey: "AIzaSyDJqkYKGXoK64qNt7D1CwMPCaC2xiEFk9o",
+authDomain: "trainschedule-cd40d.firebaseapp.com",
+databaseURL: "https://trainschedule-cd40d.firebaseio.com",
+projectId: "trainschedule-cd40d",
+storageBucket: "",
+messagingSenderId: "197604666247"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();

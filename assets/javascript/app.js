e5de46991f775091baeb8fd96d6ab7f526b9cc03
ski14hs <sean.k.ishikawa@gmail.com>
2018-09-27 21:29:57 -0700

    update to display and math for train times

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,3 +13,76 @@ messagingSenderId: "197604666247"
 firebase.initializeApp(config);
 
 var database = firebase.database();
+
+function inputTrain(){
+    event.preventDefault();
+    var name = $("#input-name").val().trim();
+    var dest = $("#input-dest").val().trim();
+    var freq = $("#input-freq").val().trim();
+    var start = $("#input-start").val().trim();
+
+    //Validate input
+    if( !name || !dest || !freq || !start){
+        //update user not valid input
+        alert("Fill in all fields");
+        return;
+    }
+    //validate start time
+    if ( start.length != 5){
+        //too long reject
+        alert("Start time must be of form HH:mm");
+        return;
+    }
+
+    //If input good push to firebase
+    var newTrain = {
+        name: name,
+        dest: dest,
+        freq: freq,
+        start: start
+    }
+    database.ref().push(newTrain);
+    $("#input-name").val("");
+    $("#input-dest").val("");
+    $("#input-freq").val("");
+    $("#input-start").val("");
+
+}
+
+$("#addTrain").on("click", inputTrain);
+
+//update html table
+database.ref().on("child_added", function(childSnapshot) {
+    // console.log(childSnapshot.val());
+  
+    // Store everything into a variable.
+    var name = childSnapshot.val().name;
+    var dest = childSnapshot.val().dest;
+    var freq = childSnapshot.val().freq;
+    var start = childSnapshot.val().start;
+  
+  
+    // Prettify the employee start
+    var startMoment = moment(start, "HH:mm");//.format("HH:mm");
+    var diffTime = moment().diff(moment(startMoment), "minutes");
+    // console.log("diff min: " + diffTime);
+    var tRemainder = Math.abs(diffTime) % freq;
+    // console.log("reminder: " + tRemainder);
+    var minTilTrain = freq - tRemainder;
+    // console.log(minTilTrain);
+    //add minTilTrain to current Time
+    var nextTrain = moment().add(minTilTrain, 'minutes').format("HH:mm");
+
+  
+    // Create the new row
+    var newRow = $("<tr>").append(
+      $("<td>").text(name),
+      $("<td>").text(dest),
+      $("<td>").text(freq),
+      $("<td>").text(nextTrain),
+      $("<td>").text(minTilTrain),
+    );
+  
+    // Append the new row to the table
+    $("#data-table > tbody").append(newRow);
+  });
